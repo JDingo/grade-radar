3a4deed2db41fb598f8d8335c8e90fc9e1dc33cc
JDingo <45366094+JDingo@users.noreply.github.com>
2019-04-25 14:34:38 +0300

    Updates

---
+++ precache-manifest.d9d5bc00b8c0c8ad0c6786dc1c33616f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "725716771a459de3b685",
+    "url": "/grade-radar/static/js/runtime~main.72571677.js"
+  },
+  {
+    "revision": "6d8530fa5ba9461aae24",
+    "url": "/grade-radar/static/js/main.6d8530fa.chunk.js"
+  },
+  {
+    "revision": "e720439cdc3590cae5f3",
+    "url": "/grade-radar/static/js/1.e720439c.chunk.js"
+  },
+  {
+    "revision": "6d8530fa5ba9461aae24",
+    "url": "/grade-radar/static/css/main.28a47c86.chunk.css"
+  },
+  {
+    "revision": "16b3479596751bbf21a1aa55da9a62c2",
+    "url": "/grade-radar/index.html"
+  }
+];
